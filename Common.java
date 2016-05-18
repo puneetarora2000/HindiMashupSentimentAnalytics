ca40c8d66444a103514b0443c1d0878c727922e3
Puneet Arora <puneetarora2000@gmail.com>
2016-05-18 17:23:59 +0530

    Add files via upload

---
+++ Common.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package mashup;
+
+/**
+ *
+ * @author Ecologic
+ */
+public class Common {
+ 
+    /**
+     *    File Paths 
+     * 
+     * */
+    
+    public final static String outputfile  = "d:\\data\\joutput.csv";
+    public final static String arfffile  = "d:\\data\\joutput.arff";
+    
+    
+    public final static String baseDir = "C:\\Users\\Ecologic\\Documents\\NetBeansProjects\\MashupSentimentAnalytics\\src\\mashup\\db\\";
+    public final static String  badenghish = baseDir+"badenghish.csv";
+    public final static String  badhinghlish = baseDir+"badhinghlish.csv";
+    
+    public final static String  goodenglish = baseDir+"goodenglish.csv";
+    public final static String  goodhinglish = baseDir+"goodhinglish.csv";
+    
+     /**
+     *    Tweeter API Data   
+     * 
+     * */
+    
+    public final static String  ConsumerKey = "fXGZ5z8OVpQdFCykmhttoPwZt";
+    public final static String  ConsumerSecret = "ShM7WiiShNEkliWedxbPlI1bas1ftMYi4ky5Q4TtOP6aoKuKhI";
+    public final static String  AccessToken = "38907556-o5Azc3OKAh847OeeBjDNYCfu2SzbAuQPuHVcIFVz2";
+    public final static String  AccessTokenSecret = "0WKlgMMSBJkFWjoLOrqwSgGPlhMJjH5tqR625nNVWotPW";
+    
+    public final static String MyPropFile = "C:\\Users\\Ecologic\\Documents\\NetBeansProjects\\MashupSentimentAnalytics\\src\\mashup\\MyPropFile.properties";
+    /**  
+     
+     StanfordCoreNLP   parameters 
+     
+     **/
+    
+    
+    public final static String  NLPparam = "";
+    
+    
+    
+}
